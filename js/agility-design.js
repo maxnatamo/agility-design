0429fc13aa95f2b44e1ae53b39c733ef06fd4844
Mads T. Kristiansen <madstk1@gmail.com>
2019-04-19 01:21:14 +0200

    Added animation-groups and fade animations.

--- js/agility-design.js
+++ js/agility-design.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Closable alerts.
     function closableReact(){
         const par = $(this).parent();
         par.css({"opacity": 0});
@@ -9,6 +10,29 @@ $(document).ready(function() {
 
     $(".closable button[action='alert-close'].close").click( closableReact );
     $(".closable button[action='callout-close'].close").click( closableReact );
+
+    // Animation groups
+    $("div.animation-group").children().each((i, v) => {
+        // Only accept elements with a .animate-* class.
+        if(~$(v).attr("class").indexOf("animate-")) {
+            
+            if(~$(v).attr("class").indexOf("animate-fade")) {
+                $(v).css( "opacity", 1 );
+            }
+
+            if(~$(v).attr("class").indexOf("animate-fade-from-left")) { $(v).css( "right", "0em" ); }
+            else if(~$(v).attr("class").indexOf("animate-fade-from-right"))  { $(v).css( "left",    "0em" ); }
+            else if(~$(v).attr("class").indexOf("animate-fade-from-top"))    { $(v).css( "bottom",  "0em" ); }
+            else if(~$(v).attr("class").indexOf("animate-fade-from-bottom")) { $(v).css( "top",     "0em" ); }
+            
+            $(v).css({
+                "transition-delay":
+                ( parseFloat( $(v).parent().attr("data-delay-initial") )
+                + parseFloat( $(v).parent().attr("data-delay-incremental") ) * i )
+                + "s"
+            });
+        }
+    });
 });
 
 $(window).scroll({ previousTop: 0 }, function () {
